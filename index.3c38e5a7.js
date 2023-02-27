037aa9434f2edcdfa4e84b09cd76a83d34e1da91
Andrii-Posternak <114352493+Andrii-Posternak@users.noreply.github.com>
2023-02-27 11:35:54 +0000

    Deploying to gh-pages from @ Andrii-Posternak/cucina-di-catherina@bbcf1e7b120c926b44040b72e55b4350cba20e5b 🚀

--- index.3c38e5a7.js
+++ index.3c38e5a7.js
@@ -1,2 +1,2 @@
 var slideIndex=1;function plusSlide(e){showSlides(slideIndex+=e)}function currentSlide(e){showSlides(slideIndex=e)}function showSlides(e){var l,s=document.getElementsByClassName("mySlides"),d=document.getElementsByClassName("dot");for(e>s.length&&(slideIndex=1),e<1&&(slideIndex=s.length),l=0;l<s.length;l++)s[l].style.display="none";for(l=0;l<d.length;l++)d[l].className=d[l].className.replace(" active","");s[slideIndex-1].style.display="block"}showSlides(slideIndex);
-//# sourceMappingURL=index.a5ecbdd1.js.map
+//# sourceMappingURL=index.3c38e5a7.js.map
